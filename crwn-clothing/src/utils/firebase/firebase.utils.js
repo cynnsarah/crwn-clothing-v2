c0d20f4051ecff2754047062ab5d243ac3de0a0c
Sarah Cynn <115136514+cynnsarah@users.noreply.github.com>
2023-04-07 12:50:02 -0700

    initializing app instance for fb connection

---
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCDiXFnpxnwZ4uW1w5SCDVTDbj8VRb_sbw",
+    authDomain: "crwn-clothing-db-7d7d7.firebaseapp.com",
+    projectId: "crwn-clothing-db-7d7d7",
+    storageBucket: "crwn-clothing-db-7d7d7.appspot.com",
+    messagingSenderId: "636811176093",
+    appId: "1:636811176093:web:935de0f1155475d135cc74"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+
